e72c3cf699eed3c603f878bdafb826c48cccc0c4
Joshua <joshuagarrison27@gmail.com>
2016-08-15 15:33:35 -0400

    Add gitignore and logging

--- BuzzerDummy.py
+++ BuzzerDummy.py
@@ -1,9 +1,10 @@
 import datetime
 import time
+import logging
+import sys
 
 
 class BuzzerDummy:
-
     def __init__(self, time='09:00', name='Untitled', duration=1, pwr_pin=23):
         self.name = name
         self.duration = duration
@@ -11,10 +12,11 @@ class BuzzerDummy:
         self.power_pin = pwr_pin
 
     def toggle(self):
-        print("Buzzer - \"" + self.name + "\" - ON - " + str(datetime.datetime.now()) + " - Power pin " + str(
-            self.power_pin) + " (Duration: " + str(self.duration) + " seconds)")
-        time.sleep(self.duration)
-        print("Buzzer - \"" + self.name + "\" - OFF - " + str(datetime.datetime.now()) + " - Power pin " + str(
-            self.power_pin))
-        print("____________________________________________________________________")
-
+        try:
+            logging.info("Buzzer - \"" + self.name + "\" - ON - " + str(datetime.datetime.now()) + " - Power pin " +
+                         str(self.power_pin) + " (Duration: " + str(self.duration) + " seconds)")
+            time.sleep(self.duration)
+            logging.info("Buzzer - \"" + self.name + "\" - OFF - " + str(datetime.datetime.now()) + " - Power pin " +
+                         str(self.power_pin))
+        except:
+            logging.critical("Unexpected error:", sys.exc_info()[0])
--- Main.py
+++ Main.py
@@ -2,21 +2,34 @@ import schedule
 import time
 import BuzzerDummy
 import json
+import logging
+
+# Set up logger
+log_format = '[%(levelname)s] - %(message)s'
+logging.basicConfig(filename='example.log', format=log_format, level=logging.DEBUG)
 
 # Read in the config file
 with open('config.json', 'r') as f:
     config = json.load(f)
+logging.info("config.json file loaded!")
 
 # Load in Buzzer Tasks from config
 buzzers = []
 for task in config['tasks']:
-    b = BuzzerDummy.BuzzerDummy(name=task['name'], time=task['time'], duration=task['buzzer_duration_secs'])
+    b = BuzzerDummy.BuzzerDummy(name=task['name'],
+                                time=task['time'],
+                                duration=task['buzzer_duration_secs'],
+                                pwr_pin=config["power_pin"])
     buzzers.append(b)
+logging.info("Tasks loaded into memory! (Total: %s)", str(len(buzzers)))
 
 # Set up the schedule
 for bt in buzzers:
     schedule.every().day.at(bt.time).do(bt.toggle)
+logging.info("Buzzer tasks loaded into scheduler!")
 
+logging.info("Starting schedule listener...")
+logging.info("Next scheduled task is at %s", schedule.next_run())
 # Begin Schedule Loop
 while True:
     schedule.run_pending()
